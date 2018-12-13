3cbea7a1a31130f8585fd9c3915984c509946c7a
Pradyuman Dixit <crvasu779@gmail.com>
2018-12-13 11:13:23 +0100

    Create first.js

---
+++ first.js
@@ -0,0 +1,62 @@
+// This is tutorial to learn JS Good Parts, and build yourself a new skill
+
+document.writeln("Hello, JS, good parts");
+
+/*
+This is how to write methods and functions in JS
+
+Function.prototype.method = function (name, func) {
+         this.prototype[name] = func;
+         return this;
+};
+
+More details about this would be in fourth.js
+
+*/
+
+/*
+
+There is a var data type that can be used for any data type of a variable
+Also, there are following keywords:
+
+**************
+abstract 
+boolean break byte
+case catch char class const continue
+debugger default delete do double
+else enum export extends
+false final finally float for function
+goto
+if implements import in instanceof int interface
+long
+native new null
+package private protected public
+return
+short static super switch synchronized
+this throw throws transient true try typeof
+var volatile void
+while with
+****************
+
+There is no Integer or int data type and hence in JS, 1 is same as 1.0
+
+This is a significant conve- nience because problems of overflow in short integers are completely avoided, and all you need to know about a number is that it is a number.
+
+A string literal can be wrapped in single quotes or double quotes. It can contain zero or more characters. The \ (backslash) is the escape character. JavaScript was built at a time when Unicode was a 16-bit character set, so all characters in JavaScript are 16 bits wide.
+
+"A" === "\u0041"
+Strings have a length property. For example, "seven".length is 5.
+Strings are immutable. Once it is made, a string can never be changed.
+But Strings are easily appended using "+" operator.
+
+Two strings containing exactly the same characters in the same order are considered to be the same string. So:
+    'c' + 'a' + 't' === 'cat'
+
+
+There are usual programming loops in JS, just like in C, C++, Java, Python or any other conventional coding language you might be knowing, 
+Same loops and syntax for while, for, do while, switch and others.
+
+
+That's it for introduction, let's move on to Objects, in second.js
+
+*/
